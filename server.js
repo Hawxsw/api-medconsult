784ec2347585c23272c91bddaa1c91a91e7bba02
Hawxsw <williamsilva1942@gmail.com>
2025-03-12 14:41:15 -0300

    fix: error

---
+++ server.js
@@ -0,0 +1,5 @@
+// Registrar ts-node para permitir a execução de arquivos TypeScript
+require('ts-node/register');
+
+// Importar e executar o arquivo TypeScript principal
+require('./src/server.ts');
